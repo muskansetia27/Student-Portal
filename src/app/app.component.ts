801944725ebc67b600d78ad9a7a321972dbff85e
muskansetia27 <muskansetia27@gmail.com>
2020-05-30 22:16:53 +0530

    student portal

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+
+ 
+
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'studentPortal';
+  
+  
+
+}
