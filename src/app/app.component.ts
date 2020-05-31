44888ea2fcf9c2b95a12cd08b812d8952559ac5b
muskansetia27 <66176412+muskansetia27@users.noreply.github.com>
2020-05-31 23:16:43 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 
+
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
-
- 
-
 import 'rxjs/add/operator/map';
+import { ModuleWithProviders, NgModule} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,37 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   title = 'studentPortal';
+  angForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    rollno: new FormControl('', Validators.required),
+    sapid: new FormControl('', Validators.required),
+    totalclasses: new FormControl('', Validators.required),
+    dayspresent: new FormControl('', Validators.required),
+  });
+
+  get name(): any {
+    return this.angForm.get('name');
+  }
+  get rollno(): any {
+    return this.angForm.get('rollno');
+  }
+  get sapid(): any {
+    return this.angForm.get('sapid');
+  } 
+  get totalclasses(): any {
+    return this.angForm.get('totalclasses');
+  }
+  get dayspresent(): any {
+    return this.angForm.get('dayspresent');
+  }
+  
+  onFormSubmit(): void {
+    console.log('Name:' + this.angForm.get('name').value);
+    console.log('RollNO:' + this.angForm.get('rollno').value);
+    console.log('Sapid:' + this.angForm.get('sapid').value);
+  }
+}
   
   
 
-}
+
