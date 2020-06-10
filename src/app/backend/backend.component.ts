c8d5cedaffe4ad811f14dabb1e72b465e0cfcfd4
muskansetia27 <66176412+muskansetia27@users.noreply.github.com>
2020-06-10 15:07:28 +0530

    Update backend.component.ts

--- src/app/backend/backend.component.ts
+++ src/app/backend/backend.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,FormControl,Validators} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import {HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-backend',
